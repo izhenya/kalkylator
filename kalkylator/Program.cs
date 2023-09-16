3055718322bdf1292670718379958e7c424cc57b
ivpav <ivpav@LAPTOP-5J7Q8KO3>
2023-09-16 14:05:43 +0300

    Добавьте файлы проекта.

---
+++ kalkylator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkylator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            char c;
+            Console.WriteLine("введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите способ расчёта (+, -, *, /)");
+            c = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+            switch (c) 
+            {
+                case '+':
+                    Console.WriteLine("{0}+{1}={2}", a ,b, a + b);
+                    break;
+                case '-':
+                    Console.WriteLine("{0}-{1}={2}", a, b, a - b);
+                    break;
+
+                    case '*':
+                    Console.WriteLine("{0}*{1}={2}", a, b, a * b);
+                    break;
+                    case '/':
+                    Console.WriteLine("{0}/{1}={2}" , a , b , a / b);
+                    break;
+                default:
+                    Console.WriteLine("ошибка");
+                    break;
+
+
+
+            }
+                
+        }
+    }
+}
