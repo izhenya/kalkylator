86fce5593ae6c7591d54a2751ff3b6b7093ff40b
Anastasia <girl-physicist@mail.ru>
2023-09-26 17:18:41 +0300

    recomend

--- kalkylator/Program.cs
+++ kalkylator/Program.cs
@@ -8,17 +8,17 @@ namespace kalkylator
 {
     internal class Program
     {
+            double a, b; // правила именований
+            char c;// следует давать значимые (передающие смысл) имена. Например typeOfTransaction
+        // в методе Main не должно быть расчетов, весь функционал выносится в отдельные методы, а тут только их вызывают
         static void Main(string[] args)
         {
-            double a, b;
-            char c;
-            Console.WriteLine("введите первое число:");
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("введите способ расчёта (+, -, *, /)");
-            c = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine("введите второе число:");
-            b = Convert.ToDouble(Console.ReadLine());
-            switch (c) 
+            GetInputData();
+           Calc();      
+        }
+        static void Calc()
+        {
+             switch (c) 
             {
                 case '+':
                     Console.WriteLine("{0}+{1}={2}", a ,b, a + b);
@@ -36,11 +36,16 @@ namespace kalkylator
                 default:
                     Console.WriteLine("ошибка");
                     break;
-
-
-
             }
-                
+        }
+        static void GetInputData()
+        {
+             Console.WriteLine("введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine()); // следовало добавить обработку исключения на случай, если не удалось конвертировать строку в число (например ввели дробное число или буквы)
+            Console.WriteLine("введите способ расчёта (+, -, *, /)");
+            c = Convert.ToChar(Console.ReadLine()); // обработка
+            Console.WriteLine("введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine()); // обработка
         }
     }
 }
